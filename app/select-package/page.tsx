f8d0c56de9a44984f44e955da436cb9aa976ae56
CodeMat17 <codemat.biz@gmail.com>
2026-07-22 23:49:34 +0100

    updated

--- app/select-package/page.tsx
+++ app/select-package/page.tsx
@@ -90,7 +90,7 @@ export default function SelectPackagePage() {
             </p>
           </div>
 
-          <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-5 mb-8">
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5 mb-8">
             {PACKAGES.map((pkg, i) => {
               const isSelected = selected === pkg.id;
               return (
